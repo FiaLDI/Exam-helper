9c49b390e40b7c2a88c6411c3817f15de8420fef
FiaLDI <leon.youtube2017@yandex.ru>
2026-02-04 18:04:39 +0300

    feat: close switchers

--- src/features/theme-switcher/ui/ThemeSwitcher.tsx
+++ src/features/theme-switcher/ui/ThemeSwitcher.tsx
@@ -2,13 +2,18 @@
 
 import { allTheme } from "@/shared/lib/theme";
 import { useThemeStore } from "../model/useThemeStore";
+import { useState } from "react";
+import { SquareArrowDown, SquareArrowUp } from "lucide-react";
 
 export function ThemeSwitcher() {
   const { theme, setTheme } = useThemeStore();
+  const [ open, setOpen ] = useState<boolean>(false);
 
   return (
-    <div className="flex flex-col lg:flex-row gap-2 fixed z-30 left-0 top-0 bg-background p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto">
-      {allTheme.map((val, idx) => (
+    <div 
+      data-isopen={open} 
+      className="flex flex-col lg:flex-row gap-2 fixed z-30 left-0 top-0 bg-background p-1 text-white text-sm [&>button]:p-2 [&>button]:cursor-pointer [&>button]:data-[current=true]:bg-blue-600 pointer-events-auto data-[isopen=false]:bg-transparent">
+      {open && allTheme.map((val, idx) => (
         <button
             key={`theme-btn-${val}-${idx}`}
             data-current={theme === val}
@@ -17,6 +22,7 @@ export function ThemeSwitcher() {
             {val}
         </button>
       ))}
+      <button onClick={()=> setOpen(prev => !prev)}>{!open ? <SquareArrowDown /> : <SquareArrowUp />}</button>
     </div>
   );
 }
