aeb8ed66a573695019d12d1f7844d966e4041214
FiaLDI <leon.youtube2017@yandex.ru>
2026-02-06 11:17:41 +0300

    feat: header burger

--- src/shared/ui/link/default/Link.tsx
+++ src/shared/ui/link/default/Link.tsx
@@ -1,3 +1,7 @@
 import Link from "next/link";
 
-export const LinkDefault = ({href, text} : {href: string; text: string}) => <li><Link href={href} className="hover:text-indigo-400">{text}</Link></li>
+export const LinkDefault = ({href, text} : {href: string; text: string}) => (
+    <li>
+        <Link href={href} className="hover:text-indigo-400">{text}</Link>
+    </li>
+)
--- src/widgets/header/ui/Header.tsx
+++ src/widgets/header/ui/Header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { LinkDefault } from "@/shared/ui/link";
-import { Menu } from "lucide-react";
+import { Cross, Menu } from "lucide-react";
 import { useState } from "react";
 
 export const Header = () => {
@@ -14,11 +14,12 @@ export const Header = () => {
     >
       <nav className="max-w-7xl mx-auto h-full p-6 py-6 flex justify-between items-center">
         <Menu className="lg:hidden text-white z-30" onClick={()=> setOpen(prev => !prev)} />
-        <ul data-open={open} className="h-full items-center gap-6 text-white data-[open=false]:hidden lg:data-[open=false]:flex lg:flex">
+        <ul data-open={open} className="h-screen z-30 p-6 lg:p-0 lg:h-full w-screen lg:w-full bg-background top-0 left-0 absolute lg:relative items-center gap-6 text-white data-[open=false]:hidden lg:data-[open=false]:flex lg:flex">
           <LinkDefault href="/" text="Главная" />
           <LinkDefault href="/exam" text="База экзаменов" />
           <LinkDefault href="/questions" text="База вопросов" />
         </ul>
+        <Cross data-open={open} className="lg:hidden absolute right-5 top-5 data-[open=false]:hidden text-white z-30" onClick={()=> setOpen(prev => !prev)} />
         <div className="flex gap-5">
             <button 
               className="text-white p-1 px-6 cursor-pointer border-2 border-indigo-400 rounded-2xl py-1 shadow-[0_0_5px_0.5px_rgba(99,102,241,0.9)] hover:text-indigo-400"
